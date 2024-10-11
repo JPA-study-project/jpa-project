60d1ea226330b505564b86671bf4ab2133793fe6
scho123 <nvmsnach@gmail.com>
2024-10-11 14:48:07 +0900

    feat: select all 기능 구현

--- src/main/java/com/spacesinspace/jpaproject/menu/controller/MenuController.java
+++ src/main/java/com/spacesinspace/jpaproject/menu/controller/MenuController.java
@@ -52,6 +52,26 @@ public class MenuController {
 	 */
 	@GetMapping("/list")
 	public String findMenuList(@PageableDefault Pageable pageable, Model model) {
+		/* page -> number, size, sort 파라미터가 Pageable 객체에 담긴다. */
+		log.info("pageable : {}", pageable);
+
+		Page<MenuDTO> menuList = menuService.findMenuList(pageable);
+
+		log.info("조회한 내용 목록 : {}", menuList.getContent());
+		log.info("총 페이지 수 : {}", menuList.getTotalPages());
+		log.info("총 메뉴 수 : {}", menuList.getTotalElements());
+		log.info("해당 페이지에 표시 될 요소 수 : {}", menuList.getSize());
+		log.info("해당 페이지에 실제 요소 수 : {}", menuList.getNumberOfElements());
+		log.info("첫 페이지 여부 : {}", menuList.isFirst());
+		log.info("마지막 페이지 여부 : {}", menuList.isLast());
+		log.info("정렬 방식 : {}", menuList.getSort());
+		log.info("여러 페이지 중 현재 인덱스 : {}", menuList.getNumber());
+
+		PagingButtonInfo paging = Pagenation.getPagingButtonInfo(menuList);
+
+		model.addAttribute("paging", paging);
+		model.addAttribute("menuList", menuList);
+
 		return "menu/list";
 	}
 	
